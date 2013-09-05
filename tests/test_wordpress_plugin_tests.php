52adee148d1762da44f11c7e97ae889e72d72a92
Jason Penney <jpenney@jczorkmid.net>
2013-09-05 16:19:31 -0400

    skip version test if version == 'master' and https wrapper unavailable

--- tests/test_wordpress_plugin_tests.php
+++ tests/test_wordpress_plugin_tests.php
@@ -25,9 +25,17 @@ class WP_Test_WordPress_Plugin_Tests extends WP_UnitTestCase {
 		//grab the requested version
 		$requested_version = getenv( 'WP_VERSION' );
 		
+                // check that we have the https wrapper
+                if ( !in_array( 'https', stream_get_wrappers() ) )
+                    $this->markTestSkipped( 'https wrapper unavailable' );
+
 		//trunk is always "master" in github terms, but WordPress has a specific way of describing it
 		//grab the exact version number to verify that we're on trunk
 		if ( $requested_version == 'master' ) {
+			// check that we have the https wrapper
+                	if ( !in_array( 'https', stream_get_wrappers() ) )
+                    		$this->markTestSkipped( 'https wrapper unavailable' );
+
 			$file = file_get_contents( 'https://raw.github.com/WordPress/WordPress/master/wp-includes/version.php' );
 			preg_match( '#\$wp_version = \'([^\']+)\';#', $file, $matches );
 			$requested_version = $matches[1];
