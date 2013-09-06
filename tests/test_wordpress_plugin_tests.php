2bb00154deefbcfe20bf8902150621543e540643
Jason Penney <jpenney@jczorkmid.net>
2013-09-06 17:16:08 -0400

    adding new unit tests

--- tests/test_wordpress_plugin_tests.php
+++ tests/test_wordpress_plugin_tests.php
@@ -25,10 +25,6 @@ class WP_Test_WordPress_Plugin_Tests extends WP_UnitTestCase {
 		//grab the requested version
 		$requested_version = getenv( 'WP_VERSION' );
 		
-                // check that we have the https wrapper
-                if ( !in_array( 'https', stream_get_wrappers() ) )
-                    $this->markTestSkipped( 'https wrapper unavailable' );
-
 		//trunk is always "master" in github terms, but WordPress has a specific way of describing it
 		//grab the exact version number to verify that we're on trunk
 		if ( $requested_version == 'master' ) {
---
+++ tests/testcase.php
@@ -0,0 +1,43 @@
+<?php
+
+if ( class_exists( 'JCP_UseGoogleLibraries' ) ) {
+	class JCP_UseGoogleLibraries_Test extends JCP_UseGoogleLibraries {
+		public static function get_instance() {
+			if ( !isset( self::$instance ) || ! is_a(self::$instance, 'JCP_UseGoogleLibraries_Test') ) {
+				self::$instance =  new JCP_UseGoogleLibraries_Test();
+			}
+			return self::$instance;
+		}
+
+		public function get_protocol_relative_supported() {
+			return $this->protocol_relative_supported;
+		}
+
+		public function get_google_scripts() {
+			return $this->google_scripts;
+		}
+	}
+}
+
+class UGL_UnitTestCase extends WP_UnitTestCase {
+
+	function setUp() {
+		parent::setUp();
+		$this->ugl = null;
+		if ( class_exists( 'JCP_UseGoogleLibraries' ) ) {
+			$this->ugl = JCP_UseGoogleLibraries_Test::get_instance();
+		} else {
+			$this->markTestSkipped('Use Google Libraries not loaded');
+		}
+	}
+	
+}
+
+class UGL_ScriptTestCase extends UGL_UnitTestCase {
+	
+	function setUp() {
+		parent::setUp();
+		$this->scripts = new WP_Scripts;
+		wp_default_scripts( $this->scripts );
+	}
+}
