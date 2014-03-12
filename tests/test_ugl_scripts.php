0f71675beeffee172486d6d4f2d5c0c11a709f1f
Jason Penney <jpenney@jczorkmid.net>
2014-03-12 17:37:10 -0400

    updated tests, fix a pass by reference issue

--- tests/test_ugl_scripts.php
+++ tests/test_ugl_scripts.php
@@ -17,28 +17,28 @@ class UGL_ScriptTests extends UGL_ScriptTestCase {
 				$prefix = 'http://';
 			}
 		}
-		$this->assertStringStartsWith($prefix, $jquery->src);
+		$this->assertStringStartsWith( $prefix, $jquery->src );
 	}
-	
+
 	function test_scripts_replaced() {
 		$scripts = $this->ugl->get_google_scripts();
-		foreach ( array_keys( $scripts ) as $handle) {
+		foreach ( array_keys( $scripts ) as $handle ) {
 			if ( $script = $this->scripts->query( $handle ) ) {
-				if ( $script->src && strpos( $script->ver, '-' ) === false) {
-					$this->assertContains('//ajax.googleapis.com/ajax/libs', 
-							$script->src, $handle + ' should be loading from google');
+				if ( $script->src && strpos( $script->ver, '-' ) === false ) {
+					$this->assertContains( '//ajax.googleapis.com/ajax/libs',
+						$script->src, $handle + ' should be loading from google' );
 				}
 			}
 		}
 	}
-	
+
 	function test_nonstandard_ver_not_replaced() {
 		$scripts = $this->ugl->get_google_scripts();
-		foreach ( array_keys( $scripts ) as $handle) {
+		foreach ( array_keys( $scripts ) as $handle ) {
 			if ( $script = $this->scripts->query( $handle ) ) {
-				if ( $script->src && strpos( $script->ver, '-' ) !== false) {
-					$this->assertNotContains('//ajax.googleapis.com/ajax/libs',
-							$script->src, $handle + ' should not be loading from google');
+				if ( $script->src && strpos( $script->ver, '-' ) !== false ) {
+					$this->assertNotContains( '//ajax.googleapis.com/ajax/libs',
+						$script->src, $handle + ' should not be loading from google' );
 				}
 			}
 		}
