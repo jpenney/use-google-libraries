7b3440d9da2fc2ff5faa70a1858e4749b186a62d
Jason Penney <jpenney@jczorkmid.net>
2014-09-05 20:01:22 +0000

    updated tests, fix a pass by reference issue
    git-svn-id: https://plugins.svn.wordpress.org/use-google-libraries/trunk@982300 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,28 +1,15 @@
 <?php
 
-/**
- * Bootstrap the testing environment
- * Uses wordpress tests (http://github.com/nb/wordpress-tests/) which uses PHPUnit
- * @package wordpress-plugin-tests
- *
- * Usage: change the below array to any plugin(s) you want activated during the tests
- *        value should be the path to the plugin relative to /wp-content/
- *
- * Note: Do note change the name of this file. PHPUnit will automatically fire this file when run.
- *
- */
+$_tests_dir = getenv( 'WP_TESTS_DIR' );
+if ( !$_tests_dir ) $_tests_dir = '/tmp/wordpress-tests-lib';
 
-$GLOBALS['wp_tests_options'] = array(
-	'active_plugins' => array( 'use-google-libraries/use-google-libraries.php' ),
-);
+require_once $_tests_dir . '/includes/functions.php';
 
-$wp_test_inc = getenv( 'WP_TESTS_DIR' );
-if ( ! $wp_test_inc ) {
-	$wp_test_inc = dirname( __FILE__ ) . '/lib';
-} else {
-	$wp_test_inc = $wp_test_inc . '/includes';
+function _manually_load_plugin() {
+	require dirname( __FILE__ ) . '/../use-google-libraries.php';
 }
+tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
-require_once $wp_test_inc . '/functions.php';
-require $wp_test_inc . '/bootstrap.php';
-require_once dirname( __FILE__ ) . '/testcase.php';
+require $_tests_dir . '/includes/bootstrap.php';
+
+require_once dirname( __FILE__ ) . '/includes/classes.php';
